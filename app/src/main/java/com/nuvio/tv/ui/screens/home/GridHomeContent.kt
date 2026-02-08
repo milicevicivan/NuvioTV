08e05aaa524709a25bf050cab6eba50ab2b74602
tapframe <85391825+tapframe@users.noreply.github.com>
2026-02-09 02:25:45 +0530

    added missing import

--- app/src/main/java/com/nuvio/tv/ui/screens/home/GridHomeContent.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/home/GridHomeContent.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
